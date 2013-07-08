fe4318ee86f00bbb89fee4e014ec6d8fa7bd6d82
Will Haldean Brown <haldean@google.com>
2013-07-07 17:26:33 -0700

    Cleanup emu.c, move more to functions.h.

--- emu.c
+++ emu.c
@@ -5,45 +5,6 @@
 
 #define ZP(x) ((uint8_t) (x))
 
-static inline size_t mem_abs(uint8_t low, uint8_t high, uint8_t off) {
-    return (uint16_t) off + (uint16_t) low + ((uint16_t) high << 8);
-}
-
-static inline size_t mem_indirect_index(cpu *m, uint8_t addr, uint8_t off) {
-    return mem_abs(m->mem[addr], m->mem[addr+1], off);
-}
-
-static inline size_t mem_indexed_indirect(cpu *m, uint8_t addr, uint8_t off) {
-    return mem_abs(m->mem[addr+off], m->mem[addr+off+1], 0);
-}
-
-// set arg MUST be 16 bits, not 8, so that add results can fit into set.
-static inline void set_flag(cpu *m, uint8_t flag, uint16_t set) {
-    if (set) {
-        m->sr |= flag;
-    } else {
-        m->sr &= ~flag;
-    }
-}
-
-static inline uint8_t get_flag(cpu *m, uint8_t flag) {
-    return (m->sr & flag) > 0;
-}
-
-static inline void set_flags(cpu *m, uint8_t val) {
-    set_flag(m, FLAG_ZERO, !val);
-    set_flag(m, FLAG_NEGATIVE, val & 0x80);
-}
-
-static inline uint8_t bcd(uint8_t val) {
-    // bcd is "binary coded decimal"; it treats the upper nibble and lower
-    // nibble of a byte each as a decimal digit, so 01011000 -> 0101 1000 -> 58.
-    // in other words, treat hex output as decimal output, so 0x58 is treated as
-    // 58. this is dumb and adds a bunch of branching to opcode interpretation
-    // that I Do Not Like.
-    return 10 * (val >> 4) + (0x0F & val);
-}
-
 #include "functions.h"
 
 void main_loop(cpu *m) {
--- functions.h
+++ functions.h
@@ -1,15 +1,45 @@
-static inline void sub(cpu *m, uint16_t r1) {
-    if (get_flag(m, FLAG_DECIMAL)) {
-        r1 = bcd(m->ac) - bcd(r1) - !get_flag(m, FLAG_CARRY);
-        set_flag(m, FLAG_OVERFLOW, r1 > 99 || r1 < 0);
+#ifndef __6502_FUNCTIONS__
+#define __6502_FUNCTIONS__
+
+#include "cpu.h"
+
+static inline size_t mem_abs(uint8_t low, uint8_t high, uint8_t off) {
+    return (uint16_t) off + (uint16_t) low + ((uint16_t) high << 8);
+}
+
+static inline size_t mem_indirect_index(cpu *m, uint8_t addr, uint8_t off) {
+    return mem_abs(m->mem[addr], m->mem[addr+1], off);
+}
+
+static inline size_t mem_indexed_indirect(cpu *m, uint8_t addr, uint8_t off) {
+    return mem_abs(m->mem[addr+off], m->mem[addr+off+1], 0);
+}
+
+// set arg MUST be 16 bits, not 8, so that add results can fit into set.
+static inline void set_flag(cpu *m, uint8_t flag, uint16_t set) {
+    if (set) {
+        m->sr |= flag;
     } else {
-        r1 = m->ac - r1 - !get_flag(m, FLAG_CARRY);
-        set_flag(m, FLAG_OVERFLOW, 0xFF00 & r1);
+        m->sr &= ~flag;
     }
-    set_flag(m, FLAG_CARRY, r1);
-    set_flag(m, FLAG_NEGATIVE, r1 & 0x80);
-    set_flag(m, FLAG_ZERO, r1 == 0);
-    m->ac = r1;
+}
+
+static inline uint8_t get_flag(cpu *m, uint8_t flag) {
+    return (m->sr & flag) > 0;
+}
+
+static inline void set_flags(cpu *m, uint8_t val) {
+    set_flag(m, FLAG_ZERO, !val);
+    set_flag(m, FLAG_NEGATIVE, val & 0x80);
+}
+
+static inline uint8_t bcd(uint8_t val) {
+    // bcd is "binary coded decimal"; it treats the upper nibble and lower
+    // nibble of a byte each as a decimal digit, so 01011000 -> 0101 1000 -> 58.
+    // in other words, treat hex output as decimal output, so 0x58 is treated as
+    // 58. this is dumb and adds a bunch of branching to opcode interpretation
+    // that I Do Not Like.
+    return 10 * (val >> 4) + (0x0F & val);
 }
 
 static inline void add(cpu *m, uint16_t r1) {
@@ -27,3 +57,19 @@ static inline void add(cpu *m, uint16_t r1) {
     set_flag(m, FLAG_NEGATIVE, r1 & 0x80);
     m->ac = r1;
 }
+
+static inline void sub(cpu *m, uint16_t r1) {
+    if (get_flag(m, FLAG_DECIMAL)) {
+        r1 = bcd(m->ac) - bcd(r1) - !get_flag(m, FLAG_CARRY);
+        set_flag(m, FLAG_OVERFLOW, r1 > 99 || r1 < 0);
+    } else {
+        r1 = m->ac - r1 - !get_flag(m, FLAG_CARRY);
+        set_flag(m, FLAG_OVERFLOW, 0xFF00 & r1);
+    }
+    set_flag(m, FLAG_CARRY, r1);
+    set_flag(m, FLAG_NEGATIVE, r1 & 0x80);
+    set_flag(m, FLAG_ZERO, r1 == 0);
+    m->ac = r1;
+}
+
+#endif
