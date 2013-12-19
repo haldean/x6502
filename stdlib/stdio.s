b4dff993c570b73d75a49fc97db9ea10c8509aaa
Will Haldean Brown <will.h.brown@gmail.com>
2013-12-18 18:08:30 -0800

    add 6502 stdlib

---
+++ stdlib/stdio.s
@@ -0,0 +1,7 @@
+; defines useful constants for writing 6502 assembler that targets x6502
+
+putc = $FF00
+getc = $FF01
+
+#define debug .byt $FC
+#define ext .byt $FF
--- test-programs/echo.s
+++ test-programs/echo.s
@@ -1,3 +1,7 @@
+; echos everything to the terminal, transforming lowercase into uppercase
+
+#include <stdio.s>
+
 ; load location of ISR and enable interrupts
         lda     #<isr
         sta     $FFFE
@@ -14,23 +18,21 @@ isr     pha
 
 ; check if char is less than 'a'
 cheka   ldx     #$60    ; == 'a' - 1
-        cpx     $FF01
-        .byt    $FC     ; dump command
-        bcs     chekz   ; if val is >= a, all good so far, skip to z check
-        jmp     noup    ; if val is < a, don't transform it
+        cpx     getc
+        bcs     noup    ; if val is < a, don't transform it
 
 ; check if char is greater than 'z'
 chekz   ldx     #$7B    ; == 'z' + 1
-        cpx     $FF01
-        .byt    $FC     ; dump command
-        bcs     noup    ; if val is >= z + 1, don't transform it
+        cpx     getc
+        bcc     noup    ; if val is >= z + 1, don't transform it
 
-        lda     #$E0    ; 'A' - 'a'
-        adc     $FF01
+; lower -> upper
+        lda     #$DF    ; 'A' - 'a'
+        adc     getc
         jmp     done
 
-noup    lda     $FF01
-done    sta     $FF00
+; print and return
+noup    lda     getc
+done    sta     putc
         pla
         rti
-        .word $0000, loop
